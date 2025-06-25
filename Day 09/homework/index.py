294662d8defb22631628d7854ccbe6f9e8c55acd
Rezi756 <kharbediarezi@gmail.com>
2025-06-25 16:06:37 +0400

    second commit

---
+++ Day 09/homework/index.py
@@ -0,0 +1,24 @@
+#inplicit conversion (implicit Type castion) es aris avtomaturi gardaqmna romelic xdeba kompilatoris an interpretatoris mier. igi xdeba mashin roca monacemta tipebi tavsebadia da riski minemaluri
+#explicit conversion (explicit type castion) es aris gardaqmna, romelic unda moxdes programistis mier. igi gamoiyeneba masin roca monacemta tipebis naklebad tavsdeba da sawiro ganmarteba
+
+#davaleba 2
+
+num = int(input("enter number: "))
+
+for i in range(0, num):
+    print(i)
+#davaleba 3
+
+num = int(input("enter number: "))
+
+while num > 0:
+    print(num)
+    num += 1
+
+
+
+
+
+
+
+
---
+++ Day 10/classwork/index.py
@@ -0,0 +1,87 @@
+# davaleba 1
+
+
+for i in range(1, 101):
+
+ i % 2 == 0
+    print(f"{i} - luwi")
+else:
+    print(f"{i} - kenti")
+
+#davaleba 2
+
+number = int(input("enter number: "))
+
+if number % 2 == 0:
+    print("this number i luwi")
+else:
+    print("this nunder kenti")
+
+    num1 = int(input("enter your first num: "))
+num2 = int(input("enter your second num: "))
+
+if num2 > num1:
+    print("metia")
+else:
+    print("naklebi")
+
+#davaleba 3
+
+
+
+num1 = int(input("enter your first num: "))
+num2 = int(input("enter your second num: "))
+
+if num1 > num2:
+    print("higer number:", num1)
+elif num2 > num1:
+    print("higer number:", num2)
+else:
+    print("both number are equal")or i in range(1, 101)
+    if i % 2 == 0:
+        print(f"{i} - luwi")
+    else:
+        print(f"{i} - kenti")
+
+
+        number = int(input("enter number: "))
+
+
+
+
+# davaleba 4
+
+
+
+if number % 2 == 0:
+    print("this number i luwi")
+else:
+    print("this nunder kenti")
+
+    num1 = int(input("enter your first num: "))
+num2 = int(input("enter your second num: "))
+
+if num2 > num1:
+    print("metia")
+else:
+    print("naklebi")
+
+
+
+# davaleba 5
+
+
+num1 = int(input("enter your first num: "))
+num2 = int(input("enter your second num: "))
+
+if num1 > num2:
+    print("higer number:", num1)
+elif num2 > num1:
+    print("higer number:", num2)
+else:
+    print("both number are equal")
+
+
+
+
+
